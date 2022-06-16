738ccaf96f69b1db77a539898e962c38f7d87f6a
PlewkaMaciej <82128422+PlewkaMaciej@users.noreply.github.com>
2022-06-16 10:39:20 +0200

    add removing option for  articles in library

--- pages/library/singleArticle.js
+++ pages/library/singleArticle.js
@@ -8,6 +8,7 @@ export const singleArticle = (singleArticle) => {
     let publishedAt = document.createElement("p");
     let newsSite = document.createElement("a");
     let removeFromLibraryButton = document.createElement("button");
+
     title.innerHTML = "Title: " + singleArticle.title
     newsSite.innerHTML = "Go to news Site " + singleArticle.newsSite
     newsSite.href = singleArticle.url
@@ -15,6 +16,8 @@ export const singleArticle = (singleArticle) => {
         summary.innerHTML = "Summary: " + singleArticle.summary.substring(0, 200)
     }
     publishedAt.innerHTML = "Published: " + singleArticle.publishedAt.substring(0, 10)
+
+
     removeFromLibraryButton.classList.add("classic-button")
     singleArticleContainer.classList.add("single-article-container");
     removeFromLibraryButton.innerText = "Remove from library"
@@ -22,14 +25,23 @@ export const singleArticle = (singleArticle) => {
     publishedAt.classList.add("classic-paragraph");
     title.classList.add("classic-paragraph");
     newsSite.classList.add("newSite-paragraph");
+
     removeFromLibraryButton.id = singleArticle.id
+    singleArticleContainer.id=singleArticle.id
+
     container.appendChild(singleArticleContainer)
     singleArticleContainer.appendChild(title)
     singleArticleContainer.appendChild(publishedAt)
     singleArticleContainer.appendChild(summary)
     singleArticleContainer.appendChild(newsSite)
     singleArticleContainer.appendChild(removeFromLibraryButton)
+    
     removeFromLibraryButton.name="isInLibrary"
+const removeContainer =(e)=>{
+    let element = document.getElementById(e.target.id);
+    element.parentNode.removeChild(element);
+}
     removeFromLibraryButton.addEventListener("click", removeFromlibrary)
+    removeFromLibraryButton.addEventListener("click", removeContainer)
 }
 
